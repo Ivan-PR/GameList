96d704ce91b371d50bdecf76e60e8ffd88dd82f3
Ivan <thos.ivanprieto@gmail.com>
2023-02-13 20:27:45 +0100

    Co-authored-by: thos-quero <thos-quero@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('home'); 
 });
