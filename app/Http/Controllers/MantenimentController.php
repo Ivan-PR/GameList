c14cbff356fc88ba2ea47e3c1639927413b69592
Ivan <thos.ivanprieto@gmail.com>
2023-02-15 20:55:32 +0100

    Route group funcionando y primera plantilla prueba

--- app/Http/Controllers/MantenimentController.php
+++ app/Http/Controllers/MantenimentController.php
@@ -7,17 +7,17 @@ use Illuminate\Http\Request;
 class MantenimentController extends Controller
 {
     public function index(){
-        return view('manteniment');
+        return view('manteniment/manteniment');
     }
 
     public function create(){
-        return view('manteniment');
+        return view('manteniment/manteniment');
     }
     public function edit(){
-        return view('manteniment');
+        return view('manteniment/manteniment');
     }
     public function delete(){
-        return view('manteniment');
+        return view('manteniment/manteniment');
     }
 }
 
