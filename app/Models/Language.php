4985a9932ffcacbd2f5a7bdd7a8fbe14575c22bc
Ivan <thos.ivanprieto@gmail.com>
2023-03-09 16:41:00 +0100

    Mutador Y Accesor Language

--- app/Models/Language.php
+++ app/Models/Language.php
@@ -4,9 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
-class Language extends Model
-{
+class Language extends Model {
     //protected $table = 'languages';
     use HasFactory;
+
+    protected function language(): Attribute {
+        return new Attribute(
+            // Mutador da formato al dato antes de ser guardado en la base de datos
+            set: fn ($value) => ucwords($value),
+            // Accesor da formato al dato antes de ser mostrado en la vista
+            get: fn ($value) => ucwords($value)
+        );
+    }
 }
