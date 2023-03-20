8722db6ff363570b01c773d9f6e10f3129294641
Adrian <thos.adrianquero@gmail.com>
2023-03-20 18:53:27 +0100

    fix

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -36,8 +36,6 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{route('manteniment.index')}}">Manteniment</a>
                     </li>
-                    @can()
-                    @endcan
                 </ul>
             </div>
         </div>
