c409ecbbe32c685214bc70577ed6a32f41491ffc
Ivan <thos.ivanprieto@gmail.com>
2023-04-17 19:34:46 +0200

    e

--- app/Http/Controllers/MantenimentLocalitzacionsController.php
+++ app/Http/Controllers/MantenimentLocalitzacionsController.php
@@ -9,6 +9,6 @@ class MantenimentLocalitzacionsController extends Controller
 {
     public function index() {
         $locations = Location::orderBy('id', 'desc')->paginate(5);
-        return view('mantenimentLocalitzacions.localitzacions.index', compact('locations'));
+        return view('mantenimentLocalitzacions.localitzacions.home', compact('locations'));
     }
 }
