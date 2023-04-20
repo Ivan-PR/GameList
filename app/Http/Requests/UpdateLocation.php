aef7588eed3baadf2b8621a9560d199f615a7574
Ivan <thos.ivanprieto@gmail.com>
2023-04-20 20:11:22 +0200

    Revert "no funciona almacenar en editar"
    This reverts commit 8e7f363e7c5b3b3b3537282c4b156557810c930e.

--- app/Http/Requests/UpdateLocation.php
+++ app/Http/Requests/UpdateLocation.php
@@ -4,13 +4,15 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateLocation extends FormRequest {
+class UpdateLocation extends FormRequest
+{
     /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
      */
-    public function authorize() {
+    public function authorize()
+    {
         return true;
     }
 
@@ -19,25 +21,10 @@ class UpdateLocation extends FormRequest {
      *
      * @return array<string, mixed>
      */
-    public function rules() {
+    public function rules()
+    {
         return [
-
-            'location' => 'required | max:30 | string',
-            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-
-        ];
-    }
-    public function attributes() {
-        return [
-            'location' => 'Nombre del pais',
-        ];
-    }
-
-    public function messages() {
-        return [
-            'location.max' => 'Nombre del pais excesivamente largo.',
-            'image.mimes' => 'El formato de la imagen no es valido.',
-            'image.max' => 'El tamaño de la imagen no es valido.',
+            //
         ];
     }
 }
