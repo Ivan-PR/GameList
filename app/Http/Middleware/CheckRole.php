0ebf0c2d5f88891e72b490668b00e48d020dce25
Adrian <thos.adrianquero@gmail.com>
2023-05-17 17:42:26 +0200

    Fixes checkroles

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -6,12 +6,11 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class CheckRole
-{
-    public function handle(Request $request, Closure $next)
-    {
-        $rol = Auth::user()->roles->first()->name; // Obtener el nombre del primer rol del usuario
-        if ($request->user() && $rol !== "Admin") {
+class CheckRole {
+    public function handle(Request $request, Closure $next) {
+
+        $user = Auth::user();
+        if ((!isset($user)) || $request->user() && Auth::user()->roles->first()->name !== "Admin") {
             abort(403, 'No tienes permiso para acceder a esta página.');
         }
 
